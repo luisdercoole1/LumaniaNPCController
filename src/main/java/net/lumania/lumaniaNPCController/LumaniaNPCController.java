2a1aa39b155e9785f82d05176f24a6e2d56053d6
luisdercoole1 <langeweile190@gmail.com>
2025-11-01 21:25:27 +0100

    1.1

--- src/main/java/net/lumania/lumaniaNPCController/LumaniaNPCController.java
+++ src/main/java/net/lumania/lumaniaNPCController/LumaniaNPCController.java
@@ -1,24 +1,10 @@
 package net.lumania.lumaniaNPCController;
 
-import it.unimi.dsi.fastutil.ints.IntArrayList;
-import it.unimi.dsi.fastutil.ints.IntList;
-import net.lumania.lumaniaNPCController.models.NPC;
-import net.lumania.lumaniaNPCController.npc.ListNPC;
 import net.lumania.lumaniaNPCController.npc.NPCListener;
 import net.lumania.lumaniaNPCController.npc.NPCManager;
-import net.minecraft.network.protocol.game.ClientboundPlayerInfoRemovePacket;
-import net.minecraft.network.protocol.game.ClientboundRemoveEntitiesPacket;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import org.bukkit.Bukkit;
-import org.bukkit.craftbukkit.v1_20_R4.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.List;
-
-import static net.lumania.lumaniaNPCController.npc.NPCManager.safeGetUUID;
-import static net.lumania.lumaniaNPCController.npc.NPCManager.safeSendPacket;
 
 public final class LumaniaNPCController extends JavaPlugin {
 
@@ -44,25 +30,11 @@ public final class LumaniaNPCController extends JavaPlugin {
 
     @Override
     public void onDisable() {
+        NPCManager.deleteNPCs();
+
         Bukkit.getConsoleSender().sendMessage(
                 "\n" +
                         "§f[LumaniaNPCController] LumaniaNPCController disabled" +
                         "\n");
-    }
-
-    public static void deleteNPCs() {
-        for (NPC npc : ListNPC.NPCs.values()) {
-            IntList npcIds = new IntArrayList();
-            npcIds.add(npc.getPlayer().getBukkitEntity().getEntityId());
-
-            ClientboundRemoveEntitiesPacket packetDestroy = new ClientboundRemoveEntitiesPacket(npcIds);
-            ClientboundPlayerInfoRemovePacket packetRemove = new ClientboundPlayerInfoRemovePacket(List.of(safeGetUUID(npc.getPlayer())));
-
-            for (Player p : Bukkit.getOnlinePlayers()) {
-                ServerGamePacketListenerImpl connection = ((CraftPlayer) p).getHandle().connection;
-                safeSendPacket(connection, packetDestroy);
-                safeSendPacket(connection, packetRemove);
-            }
-        }
     }
 }
